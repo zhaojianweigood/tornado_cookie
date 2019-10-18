67fe67557fe66ba0d4ff18aec9fd0c151c33a590
zhaojianwei <zhaojianwei@rouchi.com>
2019-10-18 17:21:19 +0800

    添加配置文件

--- cookie/settings.py
+++ cookie/settings.py
@@ -1,5 +1,23 @@
+import os
+
+APP_DIR = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
+
+base_config = {
+    'template_path': os.path.join(APP_DIR, "templates"),
+    'static_path': os.path.join(APP_DIR, "static_path"),
+}
+
+prod_config = base_config.update({
+    'debug': False,
+})
+
+dev_config = base_config.update({
+    'debug': True
+
+})
 
 config = {
-    'dev': {},
-    'prod': {},
+    'dev': dev_config,
+    'prod': prod_config,
 }
